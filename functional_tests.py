418126b8818d9680b5e566c0ba95aa497df3df66
Alex Rodriguez <alexmicrodriguez@gmail.com>
2015-03-16 17:55:09 -0500

    Second Commit, added unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		slef.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		#User goes to homepage
+		self.browser.get('http://localhost:8000')
+		
+		#User notices page title and header mention to do lists
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+
+		#User is invited to enter item right away
+if __name__=='__main__':
+	unittest.main(warnings='ignore')
