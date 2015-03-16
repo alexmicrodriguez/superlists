92ff4f3e30aebc64a60966e0e5fb5789bd226c35
alexmicrodriguez <alexmicrodriguez@gmail.com>
2015-03-15 23:13:58 -0500

    Basic view now return minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -1,22 +1,25 @@
 from selenium import webdriver
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		slef.browser.implicitly_wait(3)
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		#User goes to homepage
-		self.browser.get('http://localhost:8000')
-		
-		#User notices page title and header mention to do lists
-		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # User goes to homepage
+        self.browser.get('http://localhost:8000')
 
-		#User is invited to enter item right away
-if __name__=='__main__':
-	unittest.main(warnings='ignore')
+        # User notices page title and header mention to do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+    # User is invited to enter item right away
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
--- superlists/urls.py
+++ superlists/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'lists.views.home_page', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       url(r'^$', 'lists.views.home_page', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
+                       # url(r'^admin/', include(admin.site.urls)),
 
 )
