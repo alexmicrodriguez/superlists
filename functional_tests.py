d83c4d8b6df0eb66f10b8e4fe8cf11144ca11688
Alex Rodriguez <alexmicrodriguez@gmail.com>
2015-03-17 18:00:33 -0500

    Added Item in models

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User goes to homepage
         self.browser.get('http://localhost:8000')
@@ -34,11 +39,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('JaRule')
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy dat good gud gud', [row.text for row in rows])
-        self.assertIn('2: JaRule', [row.text for row in rows])
+        # Page updates, now displaying two items on list
+        self.check_for_row_in_list_table('1: Buy dat good gud gud')
+        self.check_for_row_in_list_table('2: JaRule')
         # there is still a text box for user to enter more items
         self.fail('Finish the test!')
 
